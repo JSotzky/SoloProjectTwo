9c8bd18111e3c108796ea60a34ea54cf42bb2544
Jason Sotzky <Jasonsotzky@gmail.com>
2022-01-14 14:50:05 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/navbar.js'
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function navbar(){
+    return (
+        <h1 className='navbar'><span><img src="./fill 213.png" /></span>my travel journal.</h1>
+    )
+}

fdec663ac54c8ac5a23566f22a5272b7713c66fe
Jason Sotzky <Jasonsotzky@gmail.com>
2022-01-15 12:58:06 -0500

    Fixed Styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function navbar(){
     return (
-        <h1 className='navbar'><span><img src="./fill 213.png" /></span>my travel journal.</h1>
+        <h1 className='navbar'><span><i class="fas fa-globe-americas fa-lg"></i></span>my travel journal.</h1>
     )
 }
